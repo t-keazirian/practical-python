9aa17d7a10e7aa360dfd36705b5b4675a765b5d8
Taylor Keazirian <tkeazirian@8thlight.com>
2022-03-17 13:59:09 -0500

    updated work from exercises 1-2.4

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100  # meters
+
+
+def print_bounces(height, num):
+    while num < 11:
+        height = height * 0.6
+        print(round(height, 4))
+        num = num + 1
+
+
+print_bounces(100, 1)
